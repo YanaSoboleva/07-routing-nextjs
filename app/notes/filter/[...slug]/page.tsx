1adf1146127480499d6d02b0445d45d685c57d97
Yana Soboleva <luxuriouslystellarsky@gmail.com>
2026-04-13 22:51:08 +0300

    Update7

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,12 +1,13 @@
-import NotesClient from '@/app/notes/NotesClient/Notes.client';
+// import NotesClient from '@/app/notes/NotesClient/Notes.client';
+import NotesClient from '../../NotesClient/Notes.client';
 
 interface FilterPageProps {
   params: { slug?: string[] };
 }
 
-export default function FilteredNotesPage({ params }: FilterPageProps) {
-  // Catch-all повертає масив, беремо перший елемент
-  const tagParam = params.slug?.[0];
+export default async function FilteredNotesPage({ params }: FilterPageProps) {
+  const resolvedParams = await params; // Додайте await
+  const tagParam = resolvedParams.slug?.[0];
   // Якщо "all", передаємо undefined, щоб бекенд повернув усе
   const activeTag = tagParam === 'all' ? undefined : tagParam;
 
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function NotesPage() {
-  // Користувач заходить на /notes -> автоматично потрапляє на /notes/filter/all
   redirect('/notes/filter/all');
 }
