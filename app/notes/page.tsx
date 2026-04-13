bcab931104513255f26f80faee16992a3067480c
Yana Soboleva <luxuriouslystellarsky@gmail.com>
2026-04-13 22:24:34 +0300

    Update6

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,18 +1,6 @@
-import { fetchNotes } from '@/lib/api';
-import NotesClient from './NotesClient/Notes.client';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { redirect } from 'next/navigation';
 
-export default async function NotesPage() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['notes', ''],
-    queryFn: () => fetchNotes({ search: '' }),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotesClient />
-    </HydrationBoundary>
-  );
+export default function NotesPage() {
+  // Користувач заходить на /notes -> автоматично потрапляє на /notes/filter/all
+  redirect('/notes/filter/all');
 }
