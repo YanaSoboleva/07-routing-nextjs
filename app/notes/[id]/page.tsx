4d4394a4bd696e8d5d391823854b6497eb4fcfa4
Yana Soboleva <luxuriouslystellarsky@gmail.com>
2026-04-17 23:11:11 +0300

    Update page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,10 +2,13 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client'; 
 
-export default async function NoteDetailsPage({ params }: { params: Promise<{ id: string }> }) {
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function NoteDetailsPage({ params }: PageProps) {
   const queryClient = new QueryClient();
   const { id } = await params;
-
 
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
